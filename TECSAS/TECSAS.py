78d97cebcd032e96f29712e0c84cf7b4a91bf367
ed29rice <esteban.dodero.rojas@rice.edu>
2025-03-15 13:43:15 -0500

    Update TECSAS.py

--- TECSAS/TECSAS.py
+++ TECSAS/TECSAS.py
@@ -990,18 +990,18 @@ class data_process:
         return test_data
 
 class PositionalEncoding(nn.Module):
-
+ 
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(0)
+ 
+        position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(1, max_len, d_model)
         pe[0, :, 0::2] = torch.sin(position * div_term)
         pe[0, :, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
-
+ 
     def forward(self, x: Tensor) -> Tensor:
         """
         Arguments:
@@ -1009,25 +1009,7 @@ class PositionalEncoding(nn.Module):
         """
         x = x + self.pe[:,:x.size(1)]
         return self.dropout(x)
-
-class PositionalEncoding_old(nn.Module):
-
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe
-        return self.dropout(x)
-
+ 
 class TECSAS(nn.Module):
     def __init__(self, n_pred_loci: int, d_model: int, nhead: int, d_hid: int,
                 nlayers: int, features: int, ostates: int, dropout: float = 0.5):
@@ -1035,26 +1017,26 @@ class TECSAS(nn.Module):
         self.model_type = 'Transformer'
         self.d_model = d_model
         self.encoder = nn.Linear(1, d_model)
-        self.pos_encoder = PositionalEncoding(d_model, dropout,max_len=features)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout, batch_first=True)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         self.fl = nn.Flatten()
         self.l2 = nn.Linear(features*d_model,ostates*(2*(n_pred_loci-1)+1))
         self.unfl = nn.Unflatten(-1,((2*(n_pred_loci-1)+1),ostates))
         self.init_weights()
-
+ 
     def init_weights(self) -> None:
         initrange = 0.1
         self.encoder.bias.data.zero_()
         self.encoder.weight.data.uniform_(-initrange, initrange)
         self.l2.bias.data.zero_()
         self.l2.weight.data.uniform_(-initrange, initrange)
-
+ 
     def partial_forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
         src = self.encoder(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
         return(src)
-
+ 
     def forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
         src = self.encoder(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
